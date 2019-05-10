b9a589d90df1091c5cbd0c1592485dca6b6f520e
jiuban0513 <571126022@qq.com>
2019-05-10 17:09:32 +0800

    update add

--- springboot_1_helloworld/src/test/java/com/boot/Springboot1HelloworldApplicationTests.java
+++ springboot_1_helloworld/src/test/java/com/boot/Springboot1HelloworldApplicationTests.java
@@ -2,15 +2,21 @@ package com.boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot1HelloworldApplicationTests {
+    //自定义属性配置
+    @Value("${an.custom.configuration}")
+    private String an;
 
     @Test
     public void contextLoads() {
+        System.out.println(this.an);
     }
 
 }
